84d677c2f9492234bcef93897babf67e35c60969
natyiassanori <natsumyiass@gmail.com>
2020-11-16 19:10:55 -0300

    Creation of ProductService

---
+++ src/main/java/com/challenge/StoreAPI/services/ProductService.java
@@ -0,0 +1,48 @@
+package com.challenge.StoreAPI.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.challenge.StoreAPI.models.Product;
+import com.challenge.StoreAPI.repositories.ProductRepository;
+
+@Service
+@Transactional
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+	
+	
+	public List<Product> listAll() {
+		return productRepository.findAll();
+	}
+	
+	public void save(Product product) {
+		productRepository.save(product);
+	}
+	
+	public Product get(int id) {
+		return productRepository.findById(id).get();
+	}
+	
+	public List<Product> getByName(String name) {
+		return productRepository.findByNameContaining(name);
+	}
+	
+	public List<Product> getByCategoryId(int categoryId) {
+		return productRepository.findByCategoryId(categoryId);
+	}
+	
+	public List<Product> findByScore(float score) {
+		return productRepository.findByScore(score);
+	}
+	
+	public void delete(int id) {
+		productRepository.deleteById(id);
+	}
+}
