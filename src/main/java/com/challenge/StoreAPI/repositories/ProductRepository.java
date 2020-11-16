c216ea0bb34ed118ae06fce0096360f0662d53ca
natyiassanori <natsumyiass@gmail.com>
2020-11-16 19:07:36 -0300

    Creation of ProductRepository

---
+++ src/main/java/com/challenge/StoreAPI/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.challenge.StoreAPI.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.challenge.StoreAPI.models.Product;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+	public List<Product> findByNameContaining(String name);
+	
+	public List<Product> findByCategoryId(int categoryId);
+	
+	public List<Product> findByScore(float score);
+}
